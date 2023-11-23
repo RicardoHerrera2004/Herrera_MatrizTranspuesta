0310b1aab9abafa7939e2c6264ada9bc85a3b18f
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-11-23 16:18:25 -0500

    Tercera version: Se crea una matriz de filas y columnas dichas por el usuario que genere numeros random

--- Matriz_Tranpuesta.c
+++ Matriz_Tranpuesta.c
@@ -1,15 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h>  //Biblioteca principal//
+#include <stdlib.h> //Biblioteca para los numeros random//
+#include <time.h>   //Biblioteca para la generacion de numeros random//
 
-int main () {
+int main()
+{ // Funcion principal//
 
-    int a,b;
-    printf ("Ingrese el valor de la fila de la matriz: ")
-    scanf ("%d", &a);
-    printf ("Ingrese el valor de la columna de la matriz: ");
-    scanf ("%d", &b);
-
+    int a, b; // Definir como enteros las variables para las filas y columnas de la matriz//
+    printf("Ingrese el valor de la fila de la matriz: ");
+    scanf("%d", &a); // Se pide y se escanea al usuario el numero de las filas//
+    printf("Ingrese el valor de la columna de la matriz: ");
+    scanf("%d", &b);            // Se pide y se escanea al usuario el numero de las columnas//
+    int matrix[a][b];           // Se define la matriz con la que vamos a trabajar //
+    srand(time(NULL));          // Se crea la funcion para crear numeros randoms//
+    for (int i = 0; i < a; i++) // Se crea el primer for para la creacion de la matriz, para el uso de filas//
+    {
+        printf("\n");               // Se crea para separar a la siguiente linea las filas//
+        for (int j = 0; j < b; j++) // Se crea el segundo for para la creacion de la matriz, para el uso de las columnas//
+        {
+            matrix[i][j] = rand() % 100 + 1; // Se define que en la matriz se agreguen numeros random en cada pocision de la matriz//
+            printf("%d \t", matrix[i][j]);   // Se imprime la matriz//
+        }
+    }
 
     return 0;
 }
