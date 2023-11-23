9ab407088e26030f59e84656b927987fc295829d
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-11-23 16:09:39 -0500

    Segunda version: Se agrega bibliotecas para los numeros randoms y se pide al usuario el numero de filas y columnas de la matriz

---
+++ Matriz_Tranpuesta.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+
+    int a,b;
+    printf ("Ingrese el valor de la fila de la matriz: ")
+    scanf ("%d", &a);
+    printf ("Ingrese el valor de la columna de la matriz: ");
+    scanf ("%d", &b);
+
+
+    return 0;
+}
